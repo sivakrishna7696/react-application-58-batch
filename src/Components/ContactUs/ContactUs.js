e67eefa5a724926f405e037930a61d9a529d6f9a
sivakrishna7696 <csivakrishna9676@gmail.com>
2022-04-29 12:58:20 +0530

    form validation

--- src/Components/ContactUs/ContactUs.js
+++ src/Components/ContactUs/ContactUs.js
@@ -6,20 +6,53 @@ class ContactUs extends React.Component {
       firstName : '',
       lastName:'',
       email:'',
-      password:''
+      password:'',
+      firstNameErr : '',
+      lastNameErr:'',
+      emailErr:'',
+      passwordErr:'',  
   }
 
   handleChange = (e) =>{
     // console.log(e)
     const {name,value} = e.target;
-    this.setState({ [name] : value},function(){
-      console.log(this.state);
-    })
+    this.setState({ [name] : value})
     console.log("afer setstate");
+    }
+
+    validateTheForm =() =>{
+      let firstNameErr ='';
+      let lastNameErr = '';
+      let emailErr = '';
+      let passwordErr = '';
+
+      if(this.state.firstName.length<4){
+        firstNameErr = "firstName should be minimum of 4 characters";
+      }
+      if(this.state.lastName.length<4){
+        lastNameErr = "lastName should be minimum of 4 characters";
+      }
+
+      if(!this.state.email.includes('@')){
+        emailErr = "Enter a valid email";
+      }
+
+      if(this.state.password.length<6){
+        passwordErr = "password should be minimum of 6 characters";
+      }
+      // falsy values
+      // 0,undefined,null,false,""
+      
+      if(firstNameErr || lastNameErr || emailErr || passwordErr){
+        this.setState({firstNameErr ,lastNameErr,emailErr,passwordErr});
+      }else{
+        this.setState({firstNameErr,lastNameErr,emailErr,passwordErr})
+      } 
     }
 
     handleSubmit = (e)=>{
       e.preventDefault();
+      this.validateTheForm();
     }
 
    render(){ 
@@ -39,40 +72,46 @@ class ContactUs extends React.Component {
                   className="form-control"
                   placeholder="Enter your FirstName"
                   name='firstName'
-                  // value={this.state.firstName}
+                  value={this.state.firstName}
                   onChange={this.handleChange}
                 />
               </div>
+              <span className="text-danger">{this.state.firstNameErr}</span>
               <div className="mb-3">
                 <input
                   type="lastName"
                   className="form-control"
                   placeholder="Enter your LastName"
                   name="lastName"
-                  // value={this.state.lastName}
+                  value={this.state.lastName}
                   onChange={this.handleChange}
                 />
               </div>
+              <span className="text-danger">{this.state.lastNameErr}</span>
+
               <div className="mb-3">
                 <input
                   type="email"
                   className="form-control"
                   placeholder="Enter your Email"
                   name="email"
-                  // value={this.state.email}
+                  value={this.state.email}
                   onChange={this.handleChange}
                 />
               </div>
+              <span className="text-danger">{this.state.emailErr}</span>
+
               <div className="mb-3">
                 <input
                   type="password"
                   className="form-control"
                   placeholder="Enter your Password"
                   name="password"
-                  // value={this.state.password}
+                  value={this.state.password}
                   onChange={this.handleChange}
                 />
               </div>
+              <span className="text-danger">{this.state.passwordErr}</span>
 
               <button className="btn btn-primary">
                 Submit
