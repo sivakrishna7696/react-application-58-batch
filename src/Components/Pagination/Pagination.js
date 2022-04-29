436d0a2e9b945676ee39f597f83f72ab64e30d1e
sivakrishna7696 <csivakrishna9676@gmail.com>
2022-04-29 10:26:00 +0530

    component lifecycle

--- src/Components/Pagination/Pagination.js
+++ src/Components/Pagination/Pagination.js
@@ -29,6 +29,28 @@ export class Pagination extends Component {
         this.getUsersData();
     }
 
+    componentDidUpdate(prevProps,prevsState){
+        console.log(prevProps,prevsState);
+      
+        if(prevsState.pageNumber !== this.state.pageNumber ){
+            this.getUsersData();
+        }
+    }
+
+    // updateUserData = (number)=>{
+
+    // }
+
+    handleButtonClick = (num)=>{
+        // console.log(num);
+        // this.updateUserData(num);
+        num = parseInt(num);
+        this.setState({pageNumber:num})
+        console.log(this.state.pageNumber)
+    }
+
+
+
   render() {
     return (
       <div>
@@ -37,18 +59,32 @@ export class Pagination extends Component {
             <div className='row'>
                 {this.state.users && this.state.users.length > 0 && this.state.users.map((user)=>(
                     // <h2>{user.name}</h2>
-                    <div className='col-md-6'>
-                        <div className='card'>
+                    <div className='col-md-6 mt-4'>
+                        <div className='card '>
                             <div className='row'>
-                                <div className='col-md-4'>
-                                    <img src={user.picture} className="img-fluid" alt="userimage" />
+                                <div className='col-md-4 p-4'>
+                                    <img src={user.picture} className="img-fluid" alt={user.title} />
                                 </div>
-                                <div className='col-md-8'></div>
+                                <div className='col-md-8'>
+                                    <div>
+                                        <p>{user.id}</p>
+                                        <p>{user.title} {user.firstName}</p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 ))}
             </div>
+          </div>
+
+          {/* pagination section */}
+          <div className="container">
+            {
+                [1,2,3,4,5,6,7,8,9].map((num)=><button className='btn btn-primary m-3' 
+                onClick={(e)=>this.handleButtonClick(num)}
+                >{num}</button>)
+            }
           </div>
       </div>
     )
